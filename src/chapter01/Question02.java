d8fd7a7c333e46c01644a9e95795994a0f6b6ce0
Kim EonJeong <0829kuj@gogofnd.com>
2024-01-25 08:57:33 +0900

    문제002, 문제003

--- src/chapter01/Question02.java
+++ src/chapter01/Question02.java
@@ -6,16 +6,15 @@ import java.util.Arrays;
 public class Question02 {
     // 002. 반복되지 않는 첫 번째 문자 찾기: 주어진 문자열에서 반복되지 않는 첫 번째 문자를 반환하는 프로그램을 작성하라.
     public static void main(String[] args) {
-        String str = "동해물과 백두산이 마르고 닳도록" +
-                "하느님이 보우하사 우리나라만세" +
-                "무궁화 삼천리 화려강산 대한사람 대한으로 길이 보전하세" +
-                "남산위에 저 소나무 철갑을 두른듯" +
-                "바람서리 불변함은 우리기상 일세" +
-                "무궁화 삼천리 화려강산 대한사람 대한으로 길이 보전하세";
+        String str = "가가가가가나나나다가가가가가가";
+        char [] chars = str.toCharArray();
 
         ArrayList arr = new ArrayList<String>();
-        for (int i=0; i < str.length(); i++) {
-
+        for (int i=0; i < chars.length; i++) {
+            if(chars[i] != chars[i+1]){
+                System.out.println("chars["+(i+1)+ "] = " + chars[i+1]);
+                break;
+            }
         }
     }
 }
---
+++ src/chapter01/Question03.java
@@ -0,0 +1,45 @@
+package chapter01;
+
+public class Question03 {
+    // 003. 글자와 단어 뒤집기: 각 단어의 글자를 뒤집는 프로그램과 각 단어의 글자와 각 단어를 뒤집는 프로그램을 작성하라.
+    public static void main(String[] args) {
+        String str1 = "사과";
+        String str2 = "바나나";
+        String str3 = "망고";
+        String str4 = "수박";
+        String str5 = "멜론";
+        String str6 = "사과 바나나 망고 수박 멜론";
+
+        System.out.println("str1 = " + wordFlipping(str1));
+        System.out.println("str2 = " + wordFlipping(str2));
+        System.out.println("str3 = " + wordFlipping(str3));
+        System.out.println("str4 = " + wordFlipping(str4));
+        System.out.println("str5 = " + wordFlipping(str5));
+
+        System.out.println(flipping(str6));
+    }
+
+    // 각 단어 뒤집기
+    private static String wordFlipping(String str) {
+        char[] chars = str.toCharArray();
+        StringBuilder newStr = new StringBuilder();
+
+        for (int i = chars.length - 1; i >= 0; i--) {
+            newStr.append(chars[i]);
+        }
+        return newStr.toString();
+    }
+
+    private static String flipping(String str) {
+        String[] stChange = str.split(" ");
+        StringBuilder newStr = new StringBuilder();
+
+        for (int i = 0; i < stChange.length; i++) {
+//            System.out.println(stChange[i]);
+            newStr.append(wordFlipping(stChange[i]));
+            newStr.append(" ");
+        }
+        return newStr.toString();
+    }
+}
+
