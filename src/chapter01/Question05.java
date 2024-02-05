82c9ca49f5e95e30f9e49c90b32776a74065cdf9
0829kuj <0829kuj@naver.com>
2024-02-05 23:09:31 +0900

    4일차 - 문제005, 문제007

--- src/chapter01/Question05.java
+++ src/chapter01/Question05.java
@@ -1,25 +1,48 @@
 package chapter01;
 
+class Result {
+    private int vowel;
+    private int consonant;
+
+    public Result(int vowel, int consonant) {
+        this.vowel = vowel;
+        this.consonant = consonant;
+    }
+
+    @Override
+    public String toString() {
+        return "Result{ " +
+                "vowel = " + vowel +
+                ", consonant = " + consonant +
+                " }";
+    }
+}
+
 public class Question05 {
     //005. 모음과 자음 세기: 주어진 문자열에서 모음과 자음 개수를 세는 프로그램을 작성하라. 대상은 자음이 5개(a, e, i, o, u)인 영어다.
     public static void main(String[] args) {
-        String str1 = "eghslejrljel";
+        String str = "eghslejrljel";
 
-        System.out.println("str1 = " + isNumbering(str1));;
+        Result result = isNumbering(str);
+        System.out.println("result.toString() = " + result.toString());
     }
 
-    private static int isNumbering(String str) {
+    private static Result isNumbering(String str) {
         char[] chars = str.toCharArray();
+        int vowel = 0;
+        int consonant = 0;
 
         for (int i = 0; i < str.length(); i++) {
-            System.out.println(chars[i]);
 
-//            if (chars[i] == "a") {
-//
-//            } else if () {
-//
-//            }
+            switch (chars[i]) {
+                case 'a', 'e', 'i', 'o', 'u' :
+                    vowel++;
+                    break;
+                default:
+                    consonant++;
+                    break;
+            }
         }
-        return chars.length;
+        return new Result(vowel, consonant);
     }
 }
---
+++ src/chapter01/Question08.java
@@ -0,0 +1,15 @@
+package chapter01;
+
+public class Question08 {
+    // 008. 문자열에서 여백 제거
+    public static void main(String[] args) {
+        String str = "가나 다 라마바 사아자차 카타 파 하 ";
+
+        String result = removeBlanks(str);
+        System.out.println("result = " + result);
+    }
+
+    private static String removeBlanks(String str) {
+        return str.replace(" ", "");
+    }
+}
